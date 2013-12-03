1740c53bea189000482c1f261c0e808463184ce7
cpldcpu <cpldcpu@gmail.com>
2013-12-03 12:41:03 +0100

    many changes, added reset state machine

--- SWD-Analyzer/swd_analyzer.c
+++ SWD-Analyzer/swd_analyzer.c
@@ -22,6 +22,9 @@ typedef unsigned long long 	qword;
 FILE             *inputfile;
 int              samplecount,totalbits,failbits;
 unsigned char    lastbyte;
+int              firstedge;
+double           timerorigin;
+double           timestamps[64];
 
 #define     CLKMASK     0x02
 #define     DATMASK     0x01
@@ -29,7 +32,7 @@ unsigned char    lastbyte;
 
 #define     SWD_RESETCOND   0xFFFFFFFFFFFFC000
 
-dword parity(dword dat) 
+unsigned int parity(unsigned int dat) 
 { 
     int ret=0;  
     for(;dat;dat>>=1) ret+=dat&1;  
@@ -82,7 +85,10 @@ char* ap_ff[]={ "","CFG/Configuration","BASE/Base Address","IDR/Identification"}
 
 int swd(qword dataRE,qword dataFE) 
 {
- byte header,ack,ret=0,t,addr,read,dap,par,datre,parre; dword dat;
+ byte header,ack,ret=0,t,addr,read,dap;
+ 
+ unsigned  int dat,par;
+ unsigned  int datre,parre; 
  char*id="";
  static byte bank;
  
@@ -100,7 +106,7 @@ if (verbose&2) {
  dap	= lsb(1,1,dataRE);
  read 	= lsb(1,2,dataRE);
  addr	= lsb(2,3,dataRE); 
- ack 	= lsb(3,8+1,dataFE);
+ ack 	= lsb(3,8+1,dataRE);
  
  if (read==1)
  {          // Read from target Trn at end
@@ -118,32 +124,71 @@ if (verbose&2) {
  
  //if(parity(header&0x7c)) return printf("parity error header\n"),1;
  
- if(!ack) return printf("Ack error %i\n",ack),1; 
+// if(!ack) return printf("Ack error %i\n",ack),1; 
  
- if (ack==7) return printf("Protocol error ack=7 \n"),1; 
+ //if (ack==7) return printf("Protocol error ack=7 \n"),8+4+1; 
  
- if(ack==2) return printf(" wait ack=2\n "),8+4+1; // wait
- if(ack==4) return printf(" fault ack=4\n"),8+4+1; // fault
+ //if (ack==7) return 8+4+1; // protcoll error, dont output
  
-// if(parity(dat)!=par) printf("data parity error");
+ if (verbose>1)
+ {
+    int i;
+    printf("@%fms: RE=",timestamps[0]);
+    for (i=0; i<(8+4+1+32+1); i++) 
+    {
+        if (i==8||i==8+4||i==9||i==8+4+(read?33:1)) printf(" ");
+        printf("%i",!!(dataRE&((long long)1<<(63-i))));        
+    }
+    printf("\n");
+    printf("@%fms: FE=",timestamps[0]);
+    for (i=0; i<(8+4+1+32+1); i++) 
+    {
+        if (i==8||i==8+4||i==9||i==8+4+(read?33:1)) printf(" ");
+        printf("%i",!!(dataFE&((long long)1<<(63-i))));        
+    }
+    printf("\n");
+ }
 
- if(dap) 				id=dap_id[(addr<<1)|(!read)];
+ 
+ if(!dap) 				id=dap_id[(addr<<1)|(!read)];
  else if(bank==0) 		id=ap_0 [addr];
  else if(bank==1) 		id=ap_1 [addr];
  else if(bank==0xff) 	id=ap_ff[addr];
  
+ 
+ if (ack==7) 
+ {
+    printf("@%fms: protocol error ack=7 - ",timestamps[0]);
+    printf("attempted: SWD(%d) %s %s  = %#x %s\n",addr, dap?"APACC":"DPACC",read?"Read":"Write", dat,id);
+    return 8+4+1+32+1;
+ } 
+ 
+  if(ack==0) 
+ {
+    printf("@%fms: protocol error ack=0 - ",timestamps[0]);
+    printf("attempted: SWD(%d) %s %s  = %#x %s\n",addr, dap?"APACC":"DPACC",read?"Read":"Write", dat,id);
+    return 8+4+1+32+1;
+ } 
+ 
+ if(ack==2) return printf("@%fms: wait ack=2\n",timestamps[0]),8+4+1; // wait
+ if(ack==4) return printf("@%fms: fault ack=4\n",timestamps[0]),8+4+1; // fault
+ 
+// if(parity(dat)!=par) printf("data parity error");
 
- printf("ack:%i SWD(%d) %s %s  = %#x %s  ",ack,addr, dap?"APACC":"DPACC",read?"Read":"Write", dat,id);
+
+ printf("@%fms: ack:%i SWD(%d) %s %s  = %#x %s  ",timestamps[0],ack,addr, dap?"APACC":"DPACC",read?"Read":"Write", dat,id);
 
  if(parity(dat)!=par) printf("Bad parity FE ");
  
- printf("dat RE=%#x ",datre);
- if(parity(datre)!=parre) printf("Bad parity RE ");
- 
+ if (read)
+ {
+    printf("dat RE=%X ",datre);
+    if(parity(datre)!=parre) printf("Bad parity RE ");
+ }
  
  printf("\n");
  
- if(dap&&addr==2&&!read) bank=(dat>>4)&0xf; // bank selection for AP access , should i check parity error ???
+ if(!dap&&addr==2&&!read) bank=(dat>>4)&0xf; // bank selection for AP access , should i check parity error ???
  
  return 8+4+1+32+1;
 }
@@ -185,6 +230,12 @@ int getnextperiod(void)
         
         if ((lastbyte^nextbyte)&CLKMASK)        // detect clk transition
         {
+        
+            if (!firstedge) {
+                firstedge=1;
+                if (verbose>0) printf("@%f: (Samplenumber: %i) First CLK transition. Using this as origin.\n",(float)samplecount/SAMPLERATE,samplecount);
+                timerorigin=(double)samplecount/SAMPLERATE;
+            }
             bitsout=(bitsout<<1) | (!!(nextbyte&DATMASK));            
             bitcount++;        
             lastbyte=nextbyte;
@@ -227,12 +278,15 @@ int main(int argc,char**argv)
     samplecount=0;
     totalbits=0;
     failbits=0;
-
+    firstedge=0;
+    
+    
      int                     discardbits=64;
      unsigned long long      bufferRE=0,bufferFE=0;
+     int                     inreset=0;   // In reset state?
      while (1)
      {
-        int inp;
+        int inp,i;
         while (discardbits-->0)
         {
             inp=getnextperiod();
@@ -240,6 +294,9 @@ int main(int argc,char**argv)
             bufferRE=(bufferRE<<1)|((inp&0x02)>>1);   // rising edge
             bufferFE=(bufferFE<<1)|(inp&0x01);   // falling edge            
             totalbits++;
+            
+            for (i=0; i<63; i++) timestamps[i]=timestamps[i+1];
+            timestamps[63]=((double)samplecount/SAMPLERATE-timerorigin)*1000;            
         }
 
         // error reading more bits   
@@ -250,24 +307,56 @@ int main(int argc,char**argv)
         // detect reset = 50 cycles while swdio=1
         if ((bufferRE&SWD_RESETCOND)==SWD_RESETCOND)
         {
-       //     if (verbose>1) printf("Reset close to: %i (%f s) - errorbits: %i\n",samplecount,(float)samplecount/SAMPLERATE,failbits);
+            if (verbose>1) printf("@%fms: Reset found. Entering reset state. Errorbits: %i\n",timestamps[0],failbits);
             discardbits=50;
             //printf("next :%X\n",(bufferRE<<50)>>(64-8));
             failbits=0;
+            inreset=1;   
             continue;
         }
+   
+                // While in reset state only Idle states or JTAG-to-SWD are recognized.             
+                // Quitting from reset state only with at least two idle states.
+        if (inreset)
+        {
+                if ((bufferRE>>(64-16))==0x79E7)    // Found magic SWJ-DP code
+                {
+                    if (verbose>1) printf("@%fms: SWJ-DP Switch\n",timestamps[0]); 
+                    discardbits=16;
+                    continue;
+                }
+                
+                // Nu-link sends 4 idle cycles
+                // ST-Link sends 2 idle cycles
+                  
+                if (!(bufferRE>>63))        // found idle cycle. Strip all idle cycles and quit reset state
+                {
+                    int idlecycles=0;
+                    unsigned long long buftemp=bufferRE;
+                    while (!(buftemp>>63)) {buftemp<<=1; idlecycles++;}
+                    
+                      if (verbose>1) printf("@%fms: %i Idle cycles. Quitting reset state.\n",timestamps[0],idlecycles); 
+                      inreset=0;
+                      discardbits=idlecycles;
+                    
+                    continue;
+                }
+                
+                discardbits=1;  // Just another reset cycle
+                continue;
+        }        
    
         // start - DAP/AP - W/R - adr[2:3] - Parity - Stop - Park - Trn - ACK W - Ack R - ACK Busy - Trn - Data32 - Parity --
         //   1		X		X		XX         P       	0		1	 Z     X         X        X       Z     X32        X
    
-        unsigned int header=bufferRE>>(64-8);
+        unsigned long header=(bufferRE>>(64-8));
   
         // Only process data if parity is correct and both start and stop bit are found.    
 //        if ((!parity(header&0x7c)) && ((header&0x82)==0x80) && (failbits<10)) 
         if ((!parity(header&0x7c)) && ((header&0x82)==0x80)) 
 //        if ((header&0x82)==0x81)
         {
-  //            if (verbose>1) printf("Valid Header %X close to: %i (%f s) - Park %i - errorbits: %i - payload %X/%X\n",header,samplecount,(float)samplecount/SAMPLERATE,!!(header&1),failbits,(unsigned int)(bufferRE>>24),(unsigned int)(bufferFE>>24));        
+              if (verbose>2) printf("Valid Header %X close to: %i (%f s) - Park %i - errorbits: %i - payload %X/%X\n",header,samplecount,(float)samplecount/SAMPLERATE,!!(header&1),failbits,(unsigned int)(bufferRE>>24),(unsigned int)(bufferFE>>24));        
                         
               discardbits=swd(bufferRE,bufferFE);
               continue;                    
